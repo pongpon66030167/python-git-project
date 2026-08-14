57d65a9d157dbdd8c2151900fc5ed77051419d52
pongpon66030167 <69030167@kmitl.ac.th>
2026-08-14 22:10:39 +0700

    fix: resolve merge conflict

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ import datetime
 
 def say_hello(name):
     now = datetime.datetime.now()
-    print(f"Goodbye, {name} from {config.APP_NAME}!")  
+    print(f"Hello again, {name} from {config.APP_NAME}!")
     print(f"Today is {now.strftime('%Y-%m-%d')}.")
 
 
